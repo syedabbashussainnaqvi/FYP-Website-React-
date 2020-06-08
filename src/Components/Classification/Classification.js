50cc8500ed85948d6a918d372404a2fca17fa99e
Abbas Naqvi <snaqvi.bscs16seecs@seecs.edu.pk>
2020-06-08 21:07:28 +0500

    Changes
    Demo Classification Done.
    Spinner Added.

--- src/Components/Classification/Classification.js
+++ src/Components/Classification/Classification.js
@@ -2,20 +2,45 @@ import React, { Component } from "react";
 import classes from "./Classification.module.css";
 import axios from "axios";
 import Upload from "../uploadComponent/upload";
-
+import Input from "../Navigation/UI/input/input";
+import Button from "../Navigation/UI/Button/Button";
 class Classification extends Component {
   constructor(props) {
     super(props);
     this.postServerHandle = this.postServerHandle.bind(this);
   }
   state = {
+    orderForm: {
+      Model: {
+        inputtype: "select",
+        elementConfig: {
+          options: [
+            { value: "S1", displayValue: "S1" },
+            { value: "S2", displayValue: "S2" },
+            { value: "S1 + S2", displayValue: "S1 + S2" },
+          ],
+        },
+        value: "S1",
+      },
+    },
     img: null,
-    avatar: null,
+    result: null,
+  };
+
+  inputHandler = (event, identifier) => {
+    const updateOrder = { ...this.state.orderForm }; //this will not create deep clone it will give access only to direct values not nested values
+    const propertyInOrder = { ...updateOrder[identifier] };
+    propertyInOrder.value = event.target.value;
+    updateOrder[identifier] = propertyInOrder;
+    this.setState({
+      orderForm: updateOrder,
+    });
   };
 
   postServerHandle = () => {
     let form = new FormData();
     form.append("file", this.state.img);
+    form.append("modelCategory", this.state.orderForm.Model.value);
 
     axios
       .post("http://127.0.0.1:8000/upload/", form)
@@ -23,7 +48,7 @@ class Classification extends Component {
         console.log(response.data);
 
         this.setState({
-          avatar: response.data.result,
+          result: response.data.result,
         });
       })
       .catch((error) => {
@@ -38,47 +63,59 @@ class Classification extends Component {
   };
 
   render() {
+    const inputs = Object.keys(this.state.orderForm).map((entry, index) => {
+      return (
+        <Input
+          key={index}
+          label={"Select Model"}
+          inputtype={this.state.orderForm[entry].inputtype}
+          elementConfig={this.state.orderForm[entry].elementConfig}
+          changeHandler={(event) => {
+            this.inputHandler(event, entry);
+          }}
+        />
+      );
+    });
+
     let btn = false;
     if (this.state.img == null) {
       btn = true;
     }
 
-    let { avatar } = this.state;
+    let { result } = this.state;
     let returnResult = null;
-    if (avatar) {
+    if (result) {
       returnResult = (
         <div className={classes.previewText2}>
-          <h1>Result</h1>
-          {this.state.avatar}
+          <h2>Result</h2>
+          <ul style={{ listStyleType: "none" }}>
+            {this.state.result.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
         </div>
       );
     }
-    // else {
-    //   $imagePreview = (
-    //     <div className={classes.previewText2}>Differenced Image</div>
-    //   );
-    // }
     let a = [classes.previewText2];
-    if (this.state.avatar) {
+    if (this.state.result) {
       a.push(classes.test);
     }
     return (
       <div className={classes.App}>
-        <h1>Upload Image For Classification</h1>
+        <h2>Upload Image For Classification</h2>
         <div className={a.join(" ")}>{returnResult}</div>
         <div className={classes.Card}>
           <div className={classes.dis}>
+            {inputs}
             <Upload setImage={this.setImage} />
           </div>
         </div>
-        <button
-          disabled={btn}
-          className={classes.submitButton}
-          type="submit"
-          onClick={this.postServerHandle}
-        >
-          Send Image To Server
-        </button>
+
+        <Button
+          result={this.state.result}
+          disable={btn}
+          postServerHandle={this.postServerHandle}
+        />
       </div>
     );
   }
---
+++ src/Components/Navigation/UI/input/input.js
@@ -0,0 +1,43 @@
+import React from "react";
+import classes from "./input.module.css";
+
+const Input = (props) => {
+  let inputElement = null;
+  switch (props.inputtype) {
+    case "input":
+      inputElement = (
+        <input
+          {...props.elementConfig}
+          className={classes.InputElement}
+          onChange={props.changeHandler}
+        />
+      );
+      break;
+    case "textArea":
+      inputElement = <textarea {...props} />;
+      break;
+    case "select":
+      inputElement = (
+        <select className={classes.select_css} onChange={props.changeHandler}>
+          {props.elementConfig.options.map((value, index) => {
+            return (
+              <option key={index} value={value.value}>
+                {value.displayValue}
+              </option>
+            );
+          })}
+        </select>
+      );
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className={classes.Input}>
+      <label className={classes.label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default Input;
