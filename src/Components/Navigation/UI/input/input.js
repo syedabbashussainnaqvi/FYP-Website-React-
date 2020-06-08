52aad0ed48fadf6f303f598d53aee19ef804ea44
Abbas Naqvi <snaqvi.bscs16seecs@seecs.edu.pk>
2020-06-08 22:30:27 +0500

    checkbox in progress

--- src/Components/Navigation/UI/input/input.js
+++ src/Components/Navigation/UI/input/input.js
@@ -29,6 +29,18 @@ const Input = (props) => {
         </select>
       );
       break;
+    case "checkbox":
+      inputElement = (
+        <label>
+          <input
+            {...props.elementConfig}
+            className={classes.InputElement}
+            onChange={props.changeHandler}
+          />
+          Check Me!
+        </label>
+      );
+      break;
     default:
       break;
   }
